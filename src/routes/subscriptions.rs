71ca4098658ca48e56a5f10a46ba4de5f792cf4e
Engin Al <enginal@gmail.com>
2024-05-31 14:37:15 +0300

    Removed unused function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -64,12 +64,3 @@ pub async fn insert_subscriber(
     })?;
     Ok(())
 }
-
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidded_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidded_characters)
-}
