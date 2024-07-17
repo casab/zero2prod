8af56dc323b18bfbd62a16b4c63f56f18c7f904b
Engin Al <enginal@gmail.com>
2024-07-18 00:54:43 +0300

    Added tests for subscription confirmations, and added email handling

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{health_check, subscribe};
+use crate::routes::{health_check, subscribe, confirm};
 
 pub struct Application {
     port: u16,
@@ -64,6 +64,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
